44f7fa561133188355de029fe92308fc97e72457
Laxodo <theernestgames@gmail.com>
2026-03-11 15:04:47 +0100

    Fixed database and database documentation.

--- doc/bd/schema.sql
+++ doc/bd/schema.sql
@@ -5,8 +5,9 @@ CREATE TABLE user(
     password VARCHAR(255) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     money DECIMAL(10, 2) NOT NULL DEFAULT 0,
-    address VARCHAR(100),
-    exchanges INT DEFAULT 0
+    opened_boosters INT NOT NULL DEFAULT 0,
+    exchanges INT DEFAULT 0,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE generation(
@@ -44,17 +45,20 @@ CREATE TABLE user_card(
     FOREIGN KEY (id_user) REFERENCES user(id)
 );
 
-CREATE TABLE transaction(
+CREATE TABLE deck(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_user_card INT NOT NULL,
-    id_from INT NOT NULL,
-    id_to INT NOT NULL,
-    date DATETIME NOT NULL,
-    price DECIMAL(10, 2),
-    type VARCHAR(20) NOT NULL,
-    FOREIGN KEY (id_user_card) REFERENCES user_card(id),
-    FOREIGN KEY (id_from) REFERENCES user(id),
-    FOREIGN KEY (id_to) REFERENCES user(id)
+    id_user INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES user(id)
+);
+
+CREATE TABLE deck_card(
+    id_deck INT NOT NULL,
+    id_card INT NOT NULL,
+    quantity INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (id_deck, id_card),
+    FOREIGN KEY (id_deck) REFERENCES deck(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_card) REFERENCES card(id) ON DELETE CASCADE
 );
 
 CREATE TABLE trade(
@@ -63,13 +67,24 @@ CREATE TABLE trade(
     id_user_card_demand INT NOT NULL,
     id_user_offer INT NOT NULL,
     id_user_demand INT NOT NULL,
-    id_transaction_offer INT NOT NULL,
-    id_transaction_demand INT NOT NULL,
-    date DATETIME NOT NULL,
+    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_user_card_offer) REFERENCES user_card(id),
     FOREIGN KEY (id_user_card_demand) REFERENCES user_card(id),
     FOREIGN KEY (id_user_offer) REFERENCES user(id),
-    FOREIGN KEY (id_user_demand) REFERENCES user(id),
-    FOREIGN KEY (id_transaction_offer) REFERENCES transaction(id),
-    FOREIGN KEY (id_transaction_demand) REFERENCES transaction(id)
+    FOREIGN KEY (id_user_demand) REFERENCES user(id)
+);
+
+CREATE TABLE transaction(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_user_card INT,
+    id_from INT NOT NULL,
+    id_to INT NOT NULL,
+    id_trade INT,
+    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    price DECIMAL(10, 2),
+    type VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_user_card) REFERENCES user_card(id),
+    FOREIGN KEY (id_from) REFERENCES user(id),
+    FOREIGN KEY (id_to) REFERENCES user(id),
+    FOREIGN KEY (id_trade) REFERENCES trade(id)
 );
